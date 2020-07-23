246b6800c3af25784eca74dc93eabe256a903ab0
Rostil Kant <rostilnytskiy@gmail.com>
2020-07-23 10:43:04 +0300

    Fourth block is completed

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +11,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
-  e = Math.E;
+  /*e = Math.E;
+  str = 'Hello Angular'
+  date = new Date()
+  float = 0.42*/
+  search = ''
+  searchField = 'title'
+  posts: Post[] = [
+    {title: 'Beer', text:'Hoegaarden'},
+    {title: 'Beer', text: 'Hike'},
+    {title: 'Blanch', text: 'Kronenburg Blank'}
+  ];
+
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolved')
+    }, 5000)
+  })
+
+  addPost() {
+    this.posts.unshift(
+      {title: 'Angular 9', text: 'Course'}
+    )
+  }
 }
