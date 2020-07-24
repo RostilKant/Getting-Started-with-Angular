65e9d325460d84bf698f9082e1e400ae48b7c341
Rostil Kant <rostilnytskiy@gmail.com>
2020-07-24 11:25:47 +0300

    Initial create

---
+++ angular-forms/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({});
+  }
+
+  submit(): void {
+    console.log('For submitted: ', this.form);
+  }
+}
