a47e3edd6a64ad6c74d5e5dfd8ccba02bdaa2780
Rostil Kant <rostilnytskiy@gmail.com>
2020-08-04 16:41:52 +0300

    Tenth block completed

--- angular-dynamic-components/src/app/app.component.ts
+++ angular-dynamic-components/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
+import {ModalComponent} from './modal/modal.component'
+import {RefDirective} from './ref.directive'
+import {Meta, Title} from '@angular/platform-browser'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-dynamic-components';
+    
+    @ViewChild(RefDirective) refDir: RefDirective
+    
+    constructor(
+        private resolver: ComponentFactoryResolver,
+        private title: Title,
+        private meta: Meta) {
+        title.setTitle('angular-dynamic')
+        meta.addTags([
+            {name: 'keywords', content: 'angular, google'},
+            {name: 'description', content: 'this is a description'},
+        ])
+    }
+    
+    showModal(): void {
+        const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+        this.refDir.containerRef.clear()
+        
+        const component = this.refDir.containerRef.createComponent(modalFactory)
+        component.instance.title = 'Modal Window'
+        component.instance.closeModal.subscribe(() => {
+            this.refDir.containerRef.clear()
+        })
+    }
+
 }
+
