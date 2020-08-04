594243a6da514e37c0f8c97366a682b9726f21fb
Rostil Kant <rostilnytskiy@gmail.com>
2020-08-04 17:11:55 +0300

    Creating PWA

--- angular-dynamic-components/src/app/modal/modal.component.ts
+++ angular-dynamic-components/src/app/modal/modal.component.ts
@@ -1,15 +1,15 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core'
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-
-  @Input() title = 'Default title'
-  @Output() closeModal = new EventEmitter<void>()
-
-  constructor() { }
-
+    
+    @Input() title = 'Default title'
+    @Output() closeModal = new EventEmitter<void>()
+    
+    constructor() { }
+    
 }
