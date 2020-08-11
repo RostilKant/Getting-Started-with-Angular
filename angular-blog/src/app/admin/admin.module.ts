ee43b3dac44847d349798c65b0d1e41fb5af8480
Rostil Kant <rostilnytskiy@gmail.com>
2020-08-11 16:48:05 +0300

    Creating main & admin layouts

---
+++ angular-blog/src/app/admin/admin.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {AdminRoutingModule} from './admin-routing.module';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+
+@NgModule({
+    declarations: [
+        AdminLayoutComponent,
+        LoginPageComponent,
+        DashboardPageComponent,
+        CreatePageComponent,
+        EditPageComponent
+    ],
+    imports: [
+        CommonModule,
+        AdminRoutingModule,
+    ],
+    exports: [],
+})
+export class AdminModule{
+
+}
--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainLayoutComponent,
+        HomePageComponent,
+        PostPageComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
