6654f69d9dc0cc30265c0f799197af51cec59e06
Rostil Kant <rostilnytskiy@gmail.com>
2020-08-12 11:09:01 +0300

    Creating authentication service

--- angular-blog/src/app/admin/admin.module.ts
+++ angular-blog/src/app/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import {AdminRoutingModule} from './admin-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from '../shared/services/auth.service';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -20,9 +23,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
         CommonModule,
         AdminRoutingModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        SharedModule
     ],
     exports: [],
+    providers: [AuthService]
 })
 export class AdminModule{
 
