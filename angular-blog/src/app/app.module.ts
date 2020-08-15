b1a9ed9c288b30047bce89e3cd649fdd2d296f6b
Rostil Kant <rostilnytskiy@gmail.com>
2020-08-15 14:45:00 +0300

    Adding PWA

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule, Provider} from '@angular/core';
 import {registerLocaleData} from '@angular/common';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import deLocale from '@angular/common/locales/de';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +13,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import {SharedModule} from './shared/shared.module';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthInterceptor} from './shared/auth.interceptor';
+import { environment } from '../environments/environment';
 
 registerLocaleData(deLocale, 'de')
 
@@ -33,7 +36,8 @@ const INTERCEPTOR: Provider = {
     imports: [
         BrowserModule,
         AppRoutingModule,
-        SharedModule
+        SharedModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [INTERCEPTOR],
     bootstrap: [AppComponent]
